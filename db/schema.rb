84ee5eb1ff0a6291ae9fbf8b89113151f18dfdc4
lauraartiles <lauraartiles@utexas.edu>
2015-07-30 16:59:13 -0400

    Added new request bootstrap icon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720233213) do
+ActiveRecord::Schema.define(version: 20150720224152) do
 
   create_table "requests", force: :cascade do |t|
     t.integer  "employee_id"
@@ -25,10 +25,11 @@ ActiveRecord::Schema.define(version: 20150720233213) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "employee_id"
+    t.integer  "employee_id"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "position"
+    t.string   "phone_number"
     t.boolean  "is_admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -42,7 +43,6 @@ ActiveRecord::Schema.define(version: 20150720233213) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
